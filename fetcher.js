4d3ccb1c81392bb41a6d18a1afb4689cc6feaec7
JP Saliba <jpsaliba@outlook.com>
2023-09-27 16:16:57 -0400

    All files committed, small little app that downloads the body of URLs into whatever local file path is specified

---
+++ fetcher.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const readline = require('readline');
+const request = require('request');
+
+const url = process.argv[2];
+const path = process.argv[3];
+
+if (process.argv.length !== 4) {
+  console.error('Please input a URL and a path (local file path) to use this module. Example: node fetcher.js <URL> <path>')
+}
+
+request(url, (error, response, body) => {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+
+  fs.writeFile(path, body, (err) => {
+    if (err) {
+      console.error(`Error: ${err}`);
+      process.exit(1);
+    }
+
+    const size = Buffer.byteLength(body, 'utf8');
+    console.log(`Downloaded and saved ${size} bytes to ${path}.`)
+  })
+});
